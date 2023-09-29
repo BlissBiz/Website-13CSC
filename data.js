6842697fbe2ee69dd35cd6bc01d43bd31b60185c
BlissBizoza <6487867-BlissBizoza@users.noreply.replit.com>
2023-09-29 07:16:55 +0000

    Initial commit

---
+++ data.js
@@ -0,0 +1,16 @@
+
+var data_setup =
+  [
+  {
+    "image": "/imgs/it-industry.jpg",
+    "industry": "Information Technology",
+    "subjects": "Maths,Computer Science,Electronics Programming",
+    "description": "The information technology (IT) sector includes companies that produce software, hardware or semiconductor equipment, and companies that provide internet or related services."
+  },
+    {
+    "image": "/imgs/it-industry.jpg",
+    "industry": "Information Technology",
+    "subjects": "Maths,Computer Science,Electronics Programming",
+    "description": "The information technology (IT) sector includes companies that produce software, hardware or semiconductor equipment, and companies that provide internet or related services."
+  },
+]
