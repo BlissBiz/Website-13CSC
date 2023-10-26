8448e39610633e863a8769558596953f6c35344a
BlissBiz <100536801+BlissBiz@users.noreply.github.com>
2023-10-26 02:40:30 +0000

    Contact development

---
+++ AboutUs/aboutUS.js
@@ -0,0 +1,30 @@
+// Gets information for Phone number and Email
+
+function submitContact() {
+  const phoneInput = document.getElementById('userPhone').value;
+  const emailInput = document.getElementById('userEmail').value;
+
+  // Check character limits
+  const phoneMinLimit = 7;
+  const phoneMaxLimit = 20;
+  const emailMinLimit = 10;
+  const emailMaxLimit = 30;
+
+
+  if (phoneInput.length < phoneMinLimit || phoneInput.length > phoneMaxLimit) {
+    alert(`Phone Number must be between ${phoneMinLimit} and ${phoneMaxLimit} characters.`);
+    return;
+  }
+
+  if (emailInput.length < emailMinLimit || emailInput.length > emailMaxLimit) {
+    alert(`Email Address must be between ${emailMinLimit} and ${emailMaxLimit} characters.`);
+    return;
+  }
+
+    alert(`Phone Number: ${phoneInput}\nEmail Address: ${emailInput}`);
+  }  
+
+  // Allow only numeric input for the phone field
+  document.getElementById('userPhone').addEventListener('input', function (e) {
+    this.value = this.value.replace(/\D/g, ''); // Remove non-numeric characters
+  });
